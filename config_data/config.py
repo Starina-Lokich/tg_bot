6122d98392511b7bd971b8c94898ee0d33436fae
Vadim <starinalokic@gmail.com>
2024-06-18 13:44:54 +0300

    Конфигурационный файл

--- config_data/config.py
+++ config_data/config.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from environs import Env
+
 
 @dataclass
 class DataBaseConfig:
@@ -17,4 +19,36 @@ class TgBot:
 @dataclass
 class Config:
     tg_bot: TgBot
-    db: DataBaseConfig 
+    db: DataBaseConfig
+
+
+# Создаем экземляр класса Env
+env: Env = Env()
+
+# Добавляем в переменные окружения данные, прочитанные из файла .env
+env.read_env()
+
+# Создаем экземпляр класса Config и наполняем его данными из переменного окружения
+config = Config(
+    tg_bot=TgBot(
+        token=env('BOT_TOKEN'),
+        admin_ids=list(map(int, env.list('ADMIN_IDS')))
+    ),
+    db=DataBaseConfig(
+        database=env('DATABASE'),
+        db_host=env('DB_HOST'),
+        db_password=env('DB_PASSWORD'),
+        db_user=env('DB_USER')
+    )
+)
+
+
+# Выводим значания полей экземпляра класса Config на печать,
+# чтобы уюедиться, что все данные, получаемые из переменных окружений, доступны
+print('BOT_TOKEN:', config.tg_bot.token)
+print('ADMIN_IDS:', config.tg_bot.admin_ids)
+print()
+print('DATABASE:', config.db.database)
+print('DB_HOST:', config.db.db_host)
+print('DB_USER:', config.db.db_user)
+print('DB_PASSWORD:', config.db.db_password)
