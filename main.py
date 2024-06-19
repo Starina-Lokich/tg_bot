735c69f959bcee152401ee5891985a9e2bc15d12
Vadim <starinalokic@gmail.com>
2024-06-19 13:06:37 +0300

    Точка входа main.py

---
+++ main.py
@@ -0,0 +1,20 @@
+import asyncio
+from aiogram import Bot, Dispatcher
+from config_data.config import Config, load_config
+
+
+# Функция конфигурирования и запуска бота 
+async def main() -> None:
+
+    # Загружаем конфигурации в переменную config
+    config: Config = load_config()
+
+    # Инициализируем Bot и Despetcher
+    bot = Bot(token=config.tg_bot.token)
+    dp = Dispatcher()
+
+    # Пропускаем накопившиеся апдейты и запускаем polling
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+asyncio.run(main())
